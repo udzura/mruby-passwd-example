6189da747cef98c7215b7620b35aae6b7d13a324
Uchio KONDO <udzura@pepabo.com>
2016-12-08 12:19:21 +0900

    Fix some warnings

--- src/mrb_passwd.c
+++ src/mrb_passwd.c
@@ -10,16 +10,19 @@
 #include <pwd.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <mruby.h>
 #include <mruby/data.h>
 #include <mruby/error.h>
+#include <mruby/class.h>
 #include "mrb_passwd.h"
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
 typedef struct mrb_passwd {
   struct passwd *pwd;
+  char *buf;
 } mrb_passwd;
 
 static void mrb_passwd_free(mrb_state *mrb, void *p)
@@ -38,12 +41,12 @@ static mrb_value mrb_passwd_init(mrb_state *mrb, mrb_value self)
   mrb_passwd *data;
   char *name;
   struct passwd pwd;
-  struct passwd *res;
   size_t bufsize;
   bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
   if (bufsize == -1)
     bufsize = 16384;
   char buf[bufsize];
+  struct passwd *res;
 
   data = (mrb_passwd *)DATA_PTR(self);
   if (data) {
@@ -54,6 +57,7 @@ static mrb_value mrb_passwd_init(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "z", &name);
   data = (mrb_passwd *)mrb_malloc(mrb, sizeof(mrb_passwd));
+  data->pwd = malloc(sizeof(struct passwd));
 
   if(getpwnam_r(name, &pwd, buf, bufsize, &res) < 0)
     mrb_sys_fail(mrb, "getpwnam failed");
@@ -61,7 +65,15 @@ static mrb_value mrb_passwd_init(mrb_state *mrb, mrb_value self)
     mrb_sys_fail(mrb, "entry not found");
   }
 
-  data->pwd = res;
+  /* data->pwd = res cannot escape struct from stack...; */
+  /* data->pwd->pw_name  = res->pw_name; */
+  /* data->pwd->pw_uid   = res->pw_uid; */
+  /* data->pwd->pw_gid   = res->pw_gid; */
+  /* data->pwd->pw_gecos = res->pw_gecos; */
+  /* data->pwd->pw_dir   = res->pw_dir; */
+  /* data->pwd->pw_shell = res->pw_shell; */
+  memcpy(data->pwd, res, sizeof(struct passwd));
+
   DATA_PTR(self) = data;
 
   return self;
@@ -83,6 +95,7 @@ void mrb_mruby_passwd_example_gem_init(mrb_state *mrb)
 {
     struct RClass *passwd;
     passwd = mrb_define_class(mrb, "Passwd", mrb->object_class);
+    MRB_SET_INSTANCE_TT(passwd, MRB_TT_DATA);
     mrb_define_method(mrb, passwd, "initialize", mrb_passwd_init, MRB_ARGS_REQ(1));
     mrb_define_method(mrb, passwd, "uid", mrb_passwd_uid, MRB_ARGS_NONE());
     mrb_define_method(mrb, passwd, "shell", mrb_passwd_shell, MRB_ARGS_NONE());
